8a4866bf067a927b34be6e0b3f6be4137afb3f84
Derek Kent <dak@dak.org>
2014-09-19 00:25:56 -0400

    Fix spacing to match style guide

--- prerender/phantom-server.js
+++ prerender/phantom-server.js
@@ -26,14 +26,14 @@ var checkComplete = function () {
     var links = document.getElementsByTagName('a');
     var reg = /^((f|ht)tps?:)?\/\//;
 
-    for(var j = 0, len = links.length; j < len; j++){
+    for (var j = 0, len = links.length; j < len; j++) {
       var anchors = links[j]
       var attr = anchors.getAttribute('href');
 
-      if(attr.match(reg)){
-        anchors.setAttribute('rel','nofollow');
-          }
+      if (attr.match(reg)) {
+        anchors.setAttribute('rel', 'nofollow');
       }
+    }
 
     console.log(content);
     phantom.exit();
